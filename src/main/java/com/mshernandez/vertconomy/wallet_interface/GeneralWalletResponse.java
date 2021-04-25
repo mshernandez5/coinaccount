4bc5c6e0f8974a293476decbb4f77ef3fa5728a6
mshernandez5 <mshernandez505@gmail.com>
2021-04-25 12:08:53 -0700

    Version and plugin metadata changes.

--- src/main/java/com/mshernandez/vertconomy/wallet_interface/GeneralWalletResponse.java
+++ src/main/java/com/mshernandez/vertconomy/wallet_interface/GeneralWalletResponse.java
@@ -1,5 +1,9 @@
 package com.mshernandez.vertconomy.wallet_interface;
 
+/**
+ * Used to deserialize basic responses
+ * without nested result objects.
+ */
 public class GeneralWalletResponse
 {
     String result;
--- src/main/java/com/mshernandez/vertconomy/wallet_interface/SmartFeeResponse.java
+++ src/main/java/com/mshernandez/vertconomy/wallet_interface/SmartFeeResponse.java
@@ -2,6 +2,10 @@ package com.mshernandez.vertconomy.wallet_interface;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Used to deserialize estimatesmartfee
+ * responses.
+ */
 public class SmartFeeResponse
 {
     Result result;
