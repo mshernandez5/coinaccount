a03cd868869a3d0859858e8bfdd581b90ed18661
mshernandez5 <mshernandez505@gmail.com>
2021-07-14 03:36:52 -0700

    Fix /deposit message creation.

--- src/main/java/com/mshernandez/vertconomy/commands/CommandDeposit.java
+++ src/main/java/com/mshernandez/vertconomy/commands/CommandDeposit.java
@@ -8,6 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
@@ -36,51 +37,37 @@ public class CommandDeposit implements CommandExecutor
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        TextComponent messageComponent = new TextComponent();
-        StringBuilder message = new StringBuilder();
         if (sender instanceof Player)
         {
-            message.append('\n');
-            message.append(ChatColor.AQUA);
-            message.append("Deposit ");
-            message.append(ChatColor.GREEN);
-            message.append(vertconomy.getSymbol());
-            message.append(ChatColor.RESET);
-            message.append('\n');
-            message.append("1) ");
-            message.append(ChatColor.AQUA);
-            message.append("Deposit To: ");
-            message.append(ChatColor.GREEN);
-            messageComponent.addExtra(message.toString());
-            // Copyable Address
+            // Copyable Deposit Address
             String address = vertconomy.getPlayerDepositAddress(((Player) sender));
             TextComponent addressMsg = new TextComponent(address);
             addressMsg.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, address));
             addressMsg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to copy address!")));
             addressMsg.setColor(ChatColor.GREEN);
-            messageComponent.addExtra(addressMsg);
-            // Show Minimum Confirmations
-            message = new StringBuilder();
-            message.append(ChatColor.RESET);
-            message.append('\n');
-            message.append("2) ");
-            message.append(ChatColor.YELLOW);
-            message.append("Wait For Your Transaction To Reach ");
-            message.append(ChatColor.LIGHT_PURPLE);
-            message.append("" + vertconomy.getMinDepositConfirmations());
-            message.append(ChatColor.YELLOW);
-            message.append(" Confirmations");
-            message.append(ChatColor.RESET);
-            message.append('\n');
+            // Build Message
+            BaseComponent[] component = new ComponentBuilder()
+                .append("\n")
+                .append("Deposit ").color(ChatColor.AQUA)
+                .append(vertconomy.getSymbol()).color(ChatColor.GREEN)
+                .append("\n").append("1) ").reset()
+                .append("Deposit To: ").color(ChatColor.AQUA)
+                .append(addressMsg).color(ChatColor.GREEN)
+                .append("\n").append("2) ").reset()
+                .append("Wait For Your Transaction To Reach ").color(ChatColor.YELLOW)
+                .append("" + vertconomy.getMinDepositConfirmations()).color(ChatColor.LIGHT_PURPLE)
+                .append(" Confirmations").color(ChatColor.YELLOW)
+                .append("\n")
+                .create();
+            sender.spigot().sendMessage(component);
         }
         else
         {
-            message.append(ChatColor.DARK_RED);
-            message.append("Unsupported");
+            BaseComponent[] component = new ComponentBuilder()
+                .append("Unsupported").color(ChatColor.DARK_RED)
+                .create();
+            sender.spigot().sendMessage(component);
         }
-
-        messageComponent.addExtra(message.toString());
-        sender.spigot().sendMessage(messageComponent);
         return true;
     }
 }
