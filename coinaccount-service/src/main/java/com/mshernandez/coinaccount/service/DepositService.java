mshernandez5/coinaccount

commit 3d3b1573cadf27ae438293aa93e69da9d6e6b386

Ignore deposits below a specified minimum, avoid accepting dust.

coinaccount-service/src/main/java/com/mshernandez/coinaccount/service/DepositService.java
@@ -18,6 +18,7 @@ import com.mshernandez.coinaccount.entity.DepositType;
 import com.mshernandez.coinaccount.entity.WithdrawRequest;
 import com.mshernandez.coinaccount.service.wallet_rpc.WalletService;
 import com.mshernandez.coinaccount.service.wallet_rpc.exception.WalletRequestException;
+import com.mshernandez.coinaccount.service.wallet_rpc.parameter.ListUnspentQuery;
 import com.mshernandez.coinaccount.service.wallet_rpc.result.ListUnspentUTXO;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -36,6 +37,9 @@ public class DepositService
 {
     @ConfigProperty(name = "coinaccount.internal.account")
     UUID internalAccountId;
+
+    @ConfigProperty(name = "coinaccount.deposit.minimum")
+    long minDepositAmount;
 
     @ConfigProperty(name = "coinaccount.deposit.confirmations")
     int minDepositConfirmations;
@@ -77,7 +81,11 @@ public class DepositService
         long addedBalance = 0L;
         long unconfirmedBalance = 0L;
         // Get UTXOs For Account
-        List<ListUnspentUTXO> utxos = walletService.listUnspent(account.getDepositAddress());
+        ListUnspentQuery utxoQuery = new ListUnspentQuery()
+            .setMinConfirmations(minDepositConfirmations)
+            .setAddresses(account.getDepositAddress())
+            .setMinimumAmount(minDepositAmount);
+        List<ListUnspentUTXO> utxos = walletService.listUnspent(utxoQuery);
         // Avoid Reprocessing Previous Transactions
         Set<String> oldTXIDs = account.getProcessedDepositIDs();
         Set<String> unspentTXIDs = new HashSet<>();

coinaccount-api/src/main/proto/coinaccount_service.proto
@@ -51,6 +51,7 @@ message GetDepositConfigurationResponse
 {
     ResponseType response_type = 1;
     uint32 min_deposit_confirmations = 2; // The minimum number of confirmations to consider a deposit valid.
+    uint64 min_deposit_amount = 3; // The minimum amount for the deposit not to be ignored.
 }
 
 /**

coinaccount-service/src/main/java/com/mshernandez/coinaccount/CoinAccountController.java
@@ -27,6 +27,9 @@ public class CoinAccountController extends CoinAccountServiceImplBase
     @ConfigProperty(name = "coinaccount.coin.base.symbol")
     String coinBaseUnitSymbol;
 
+    @ConfigProperty(name = "coinaccount.deposit.minimum")
+    int minDepositAmount;
+
     @ConfigProperty(name = "coinaccount.deposit.confirmations")
     int minDepositConfirmations;
 
@@ -75,6 +78,7 @@ public class CoinAccountController extends CoinAccountServiceImplBase
         GetDepositConfigurationResponse response = GetDepositConfigurationResponse.newBuilder()
             .setResponseType(ResponseType.SUCCESS)
             .setMinDepositConfirmations(minDepositConfirmations)
+            .setMinDepositAmount(minDepositAmount)
             .build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();

coinaccount-service/src/main/resources/application.properties
@@ -56,6 +56,13 @@ coinaccount.wallet.user: vtcuser
 coinaccount.wallet.pass: vtcpass
 
 ############################################################################
+# Default Web Interface Configuration
+############################################################################
+
+# HTTP Server Port
+quarkus.http.port: 5050
+
+############################################################################
 # Default Currency Representation
 ############################################################################
 
@@ -68,6 +75,9 @@ coinaccount.coin.base.symbol: vertoshi
 ############################################################################
 # Default UTXO Management
 ############################################################################
+
+# Minimum Amount For Deposit Not To Be Ignored
+coinaccount.deposit.minimum: 50000000
 
 # Minimum Number Of Confirmations To Consider User Deposit UTXOs Valid
 coinaccount.deposit.confirmations: 6

commit fd46490a6e64204422fdaeec9703e87eefd2aa43

Fix issue discovering pending balances caused by 3d3b157, don't filter UTXOs by confirmations since unconfirmed UTXOs must be listed to update pending balances.

coinaccount-service/src/main/java/com/mshernandez/coinaccount/service/DepositService.java
@@ -82,7 +82,6 @@ public class DepositService
         long unconfirmedBalance = 0L;
         // Get UTXOs For Account
         ListUnspentQuery utxoQuery = new ListUnspentQuery()
-            .setMinConfirmations(minDepositConfirmations)
             .setAddresses(account.getDepositAddress())
             .setMinimumAmount(minDepositAmount);
         List<ListUnspentUTXO> utxos = walletService.listUnspent(utxoQuery);